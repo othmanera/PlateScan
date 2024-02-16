45f88470323b43e4ac89b888f7b409b50c7376db
othmanera <othmanera3@gmail.com>
2024-02-16 04:31:09 +0100

    plate detection added

---
+++ main.py
@@ -0,0 +1,63 @@
+import cv2
+import numpy as np
+from ultralytics import YOLO
+from sort.sort import *
+from util import *
+
+
+# tracker
+vehicule_tracker = Sort()
+
+
+#models
+yolo_Model= YOLO('yolov8n.pt') #default cocoset YOLO model
+license_plate_detector = YOLO('best.pt') #custom model for license plate detection
+
+# load video
+cap = cv2.VideoCapture()
+
+vehicules = [2,3,5,7]
+#read frames
+frame_nbr = -1
+ret = True
+
+
+
+
+
+while ret:
+    frame_nbr += 1
+    ret, frame = cap.read()
+    if ret and frame_nbr>10:
+        pass
+
+        # vehicules detections
+
+        detections = yolo_Model(frame)
+        detections_ = []
+        for detection in detections.boxes.data.tolist():
+            x1,y1,x2,y2,score,class_id= detection
+            if int(class_id) in vehicules:
+                detections_.append([x1,y1,x2,y2,score])
+
+        # tracking vehicules
+        track_ids = vehicule_tracker.update(np.asarray((detections_)))
+
+        # detect license plate
+        license_plates = license_plate_detector(frame)[0]
+        for license_plate in license_plates.boxes.data.tolist():
+            x1, y1, x2, y2, score, class_id = license_plate
+
+
+        # assign a license plate to a tracked vehicule
+        xcar1,ycar2,xcar2,ycar2,car_id = getCar((license_plate,track_ids))
+
+        # crop license plate
+        LP_crop = frame[int(y1):int(y2), int(x1):int(x2),:]
+
+        #process license plate
+        license_plate_crop_gray = cv2.cvtColor(LP_crop, cv2.COLOR_BGR2GRAY)
+        _, license_plate_crop_thresh = cv2.threshold(license_plate_crop_gray, 64, 255, cv2.THRESH_BINARY_INV)
+
+        cv2.imshow('original_crop',LP_crop)
+        cv2.imshow('threshold', LP_crop)
---
+++ util.py
@@ -0,0 +1,3 @@
+
+def getCar(param):
+    pass
